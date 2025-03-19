1181c26214c55f6a5ed0fc76fde39eae089418f1
D <htehran333@gmail.com>
2025-03-19 14:41:32 +0330

    Update

--- src/comps/Company/About_Numbers.js
+++ src/comps/Company/About_Numbers.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { about } from "../../data/Constants";
 import { Count_Animation } from "../Portal";
 
@@ -8,17 +9,17 @@ const About_Numbers = () => {
         {about.numbers.map((item, index) => {
           const { count, elementRef } = Count_Animation(item.number, 1500);
           return (
-            <div key={index}>
+            <React.Fragment key={index}>
               <div className="text-center" ref={elementRef}>
                 <h6 className="text-blue-500 title-lg">{count}+</h6>
                 <h4 className="md:text-sm text-[10px] md:mt-3 mt-1">
                   {item.title}
                 </h4>
               </div>
-              {index !== about.numbers.length && (
+              {index !== about.numbers.length - 1 && (
                 <div className="w-[1px] bg-slate-950 h-full mx-auto bg-opacity-10" />
               )}
-            </div>
+            </React.Fragment>
           );
         })}
       </div>
