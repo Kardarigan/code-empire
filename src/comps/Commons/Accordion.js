7db98d2ec87d310139eb7b46cd91f4f509e2777a
D <htehran333@gmail.com>
2025-03-26 22:42:06 +0330

    update

--- src/comps/Commons/Accordion.js
+++ src/comps/Commons/Accordion.js
@@ -5,19 +5,19 @@ const Accordion = ({ question }) => {
   return (
     <div
       className={`w-full border-2 transition-[max-height] duration-200 rounded-xl px-5 py-3 border-blue-400 overflow-hidden ${
-        open ? "max-h-80" : "h-[79px]"
+        open ? "max-h-80" : "max-h-[79px]"
       }`}
       onClick={() => setOpen(!open)}
     >
       <div className="flex-seperate gap-x-5 h-[46px]">
-        <h5>{question.quiz}</h5>
+        <h5 className="select-none">{question.quiz}</h5>
         <i
           className={`fas fa-chevron-down text-blue-400 transition-all duration-300 ${
             open ? "rotate-180" : ""
           }`}
         ></i>
       </div>
-      <div className="py-3 relative top-[80px]">
+      <div className="py-3 relative top-[0]">
         <hr className="mt-2 mb-4" />
         <p className="para">{question.answer}</p>
       </div>
